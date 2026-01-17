70204a8adae25eafae399ecbf2a61f501da826a1
Victor Paiva <victor_eduardof@hotmail.com>
2026-01-17 21:28:03 +0000

    chore: first commit. Add movimento do bispo, torre e rainha usando while, do while e for respectivamente.

--- xadrez.c
+++ xadrez.c
@@ -1,21 +1,42 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
-
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    const int movimentoBispo = 5;
+    const int movimentoTorre = 5;
+    const int movimentoRainha = 8;
+    int contador = 0;
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    // Implementação de Movimentação do Bispo = 5 casas na diagonal superior direita
+    printf("Movimento do Bispo: %d casas.\n", movimentoBispo);
+    // movimentação com while
+    while (contador < movimentoBispo){
+        printf("Superior direita \n");
+        contador ++;
+    }
+    //zerando contador para proxima peça e saltando linha
+    contador = 0;
+    printf("\n");
+    
+    // Implementação de Movimentação da Torre = 5 casas para a direita
+    printf("Movimento da Torre: %d casas.\n", movimentoTorre);
+    // movimentação com do while
+    do {
+        printf("Direita \n");
+        contador++;
+    } while (contador < movimentoTorre);
+    //saltando linha
+    printf("\n");
+    
+    // Implementação de Movimentação da Rainha = 8 casas para a esquerda
+    printf("Movimento da Rainha: %d casas. \n", movimentoRainha);
+    // movimentação com for
+    for (contador=0; contador < movimentoRainha; contador++){
+        printf("Esquerda \n");
+    }
+    //pulando linha
+    printf("\n");
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.

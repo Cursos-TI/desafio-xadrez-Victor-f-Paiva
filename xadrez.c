203a9c2b1cb73f3d6366d30265ee9b4f201ca5b7
Victor Paiva <victor_eduardof@hotmail.com>
2026-01-17 22:06:55 +0000

    feat: add movimento do cavalo com loop aninhado, usando for e do while

--- xadrez.c
+++ xadrez.c
@@ -12,7 +12,8 @@ int main() {
     printf("Movimento do Bispo: %d casas.\n", movimentoBispo);
     // movimentação com while
     while (contador < movimentoBispo){
-        printf("Superior direita \n");
+        printf("Cima \n");
+        printf("Direita \n");
         contador ++;
     }
     //zerando contador para proxima peça e saltando linha
@@ -35,12 +36,29 @@ int main() {
     for (contador=0; contador < movimentoRainha; contador++){
         printf("Esquerda \n");
     }
+    //pulando linha e zerando contador
+    contador = 0;
+    printf("\n");
+    
+    // Nível Aventureiro - Movimentação do Cavalo = 2 casas para baixo e 1 casa para a esquerda
+    printf("Movimento do Cavalo: 3 casas. \n");
+    //loop externo for faz o movimento de 1 salto
+    for (contador=0; contador <2; contador++){ 
+        // loop interno 'do while' faz movimento de 2 saltos
+        do {
+            printf("Baixo \n");
+            contador ++;
+        } while (contador <2);       
+        printf("Esquerda \n");
+    }
+    /*
+    OBS SOBRE O CAVALO
+    para que o movimento de 1 salto seja antes do movimento de 2 saltos, 
+    o printf do loop externo deve estar antes do loop 'do while'.
+    Caso deixe como está o movimento de 1 salto será depois do movimento de 2 saltos
+    */ 
     //pulando linha
     printf("\n");
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
